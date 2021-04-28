06347882c59406065e166616e08bce65c7da6469
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2021-04-28 17:32:55 +0000

    Add all Movie categories to firestore and fetch them from firestore to our homepage

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -1,16 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+
+import db from "../../firebase/firebase";
+import { setMovies } from "../../features/movie/movieSlice";
+import { selectUserName } from "../../features/user/userSlice";
 
 import ImageSlider from "../ImageSlider/ImageSlider.component";
 import Viewers from "../Viewers/Viewers.component";
+import Recommends from "../Recommends/Recommends.component";
+import NewDisney from "../NewDisney/NewDisney.component";
+import Originals from "../Originals/Originals.component";
+import Trending from "../Trending/Trending.component";
 
 import HomeBackground from "../../assets/images/home-background.png";
 
 const Home = (props) => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trendings = [];
+
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+          case "trending":
+            trendings = [...trendings, { id: doc.id, ...doc.data() }];
+            break;
+          default:
+            break;
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trendings,
+        })
+      );
+    });
+  }, [userName]);
+
   return (
     <Container>
       <ImageSlider />
       <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   );
 };
